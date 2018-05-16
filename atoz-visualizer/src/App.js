d3115780f17ef3a7c8897bac970db0e3f95219ff
Shutaro Takimoto <shu@tak.im>
2018-05-17 00:59:17 +0900

    feat: recharts を使ってグラフ描画を確認

--- atoz-visualizer/src/App.js
+++ atoz-visualizer/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { LineChart, Line } from 'recharts';
 import './App.css';
+
+const APP_NAME = 'AtoZ VISUALIZER';
+
+function Header({title}) {
+  return (
+    <header className="App-header">
+      <h1 className="App-title">{title}</h1>
+    </header>
+  );
+}
+
+const testData = [
+  {key: 'A', down: 0, up: 50},
+  {key: 'B', down: 100, up: 120},
+  {key: 'C', down: 132, up: 145},
+  {key: 'D', down: 160, up: 180},
+  {key: 'E', down: 170, up: 200},
+  {key: 'F', down: 350, up: 420},
+  {key: 'G', down: 390, up: 420},
+  {key: 'H', down: 410, up: 420},
+];
+
+function TestChart({data}) {
+  return (
+    <LineChart width={400} height={400} data={data}>
+      <Line type="monotone" dataKey="down" stroke="#8884d8" />
+    </LineChart>
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header title={APP_NAME} />
+        <TestChart data={testData} />
       </div>
     );
   }
