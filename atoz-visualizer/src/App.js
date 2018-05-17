c70e99d36e321b18dd76b2caea628eec39bcadfe
Shutaro Takimoto <shu@tak.im>
2018-05-17 16:41:07 +0900

    wip: Heatmap

--- atoz-visualizer/src/App.js
+++ atoz-visualizer/src/App.js
@@ -28,30 +28,80 @@ function setupData(data) {
     press: 0,
     prevDown: 0,
     prevUp: 0,
-    downDiff: 0,
-    upDiff: 0
+    delta: 0,
+    kpm: 0,
   };
   return data.map((d, i) => {
-    const ext = Object.assign({}, d, defaultExt);
+    const ext = {};
     ext.press = d.up - d.down;
     if (i > 0) {
       ext.prevDown = data[i - 1].down;
       ext.prevUp = data[i - 1].up;
-      ext.downDiff = d.down - ext.prevDown;
-      ext.upDiff = d.up - ext.prevUp;
+      ext.delta = d.down - ext.prevDown;
+      ext.kpm = 60 * 1000 / ext.delta;
     }
-    return ext;
+    return {...d, ...defaultExt, ...ext};
+  });
+}
+
+function average(array) {
+  const sum = array.reduce((p, n) => p + n);
+  return sum / array.length;
+}
+
+function standardScore(n, avg, sd) {
+  return 50 + 10 * (n - avg) / sd;
+}
+
+function statsData(data) {
+  if (!data || !data.length) return [];
+  const keys = Object.keys(data[0]);
+  const statKeys = keys.filter(k => typeof data[0][k] === 'number');
+  const stats = {};
+  const cache = {};
+
+  for (let k of statKeys) {
+      cache[k] = data.map(d => d[k])
+                     .sort(((a, b) => Number(a) - Number(b)));
+      const avg = average(cache[k]);
+      const medianPos = (cache[k].length - 1) / 2;
+
+      stats[k] = {
+        average: avg,
+        median: average(cache[k].slice(Math.floor(medianPos), Math.ceil(medianPos) + 1)),
+        variance: average(cache[k].map(n => n * n)) - avg * avg,
+      };
+      stats[k].sd = Math.sqrt(stats[k].variance);
+  }
+
+  return data.map(d => {
+    const ext = {
+      score: {},
+      rank: {},
+      percentile: {}
+    };
+
+    for (let k of statKeys) {
+      ext.score[k] = standardScore(d[k], stats[k].average, stats[k].sd);
+      ext.rank[k] = cache[k].findIndex(e => e === d[k]);
+      ext.percentile[k] = data.length > 1 ?
+                        100 * ext.rank[k] / (data.length - 1):
+                        0;
+    }
+
+    return {...d, ...ext};
   });
 }
 
 function GanttChart({data}) {
+  // FIXME: label number is wrong
   return (
     <BarChart data={setupData(data)} layout="vertical" width={1000} height={400} barCategoryGap="4">
       <YAxis type="category" dataKey="key" axisLine={false} tickLine={false} />
       <XAxis type="number" allowDecimals={false} tickCount={10} />
       <CartesianGrid horizontal={false} stroke="#ccc" strokeDasharray="5 5" />
       <Bar dataKey="prevDown" stackId="a" fill="transparent" isAnimationActive={false} />
-      <Bar dataKey="downDiff" stackId="a" label={{ fill: 'white', fontSize: 9}} fill="darkslategray" stroke="darkslategray" isAnimationActive={false} />
+      <Bar dataKey="delta" stackId="a" label={{ fill: 'white', fontSize: 9}} fill="darkslategray" stroke="darkslategray" isAnimationActive={false} />
       <Bar dataKey="press" stackId="a" label={{ fill: 'gray', fontSize: 9}} fill="transparent" stroke="darkslategray" isAnimationActive={false} />
     </BarChart>
   );
@@ -65,7 +115,6 @@ class TimelineLabel extends Component {
     const height = this.props.height;
     const text = this.props.content.props.text ? this.props.content.props.text : '';
     const name = this.props.content.props.name;
-    console.log(this.props.content);
 
     return (
       <text x={x} y={y - 2} width={width} height={height} fill="darkslategray" fontSize="16" className="recharts-text recharts-label" textAnchor="middle">
@@ -79,7 +128,7 @@ class TimelineLabel extends Component {
 function TimelineChart({data}) {
   const extData = setupData(data);
   const flatData = extData.reduce((prev, current) => {
-    prev[current.key] = current.downDiff;
+    prev[current.key] = current.delta;
     return prev;
   }, {});
 
@@ -98,6 +147,22 @@ function TimelineChart({data}) {
   );
 }
 
+class Heatmap extends Component {
+  render() {
+    const data = statsData(setupData(this.props.data));
+    console.dir(data);
+    return (
+      <div>
+        {
+          data.map(d => 
+            <span key={d.key} style={{fontSize: d.percentile.kpm}}>{d.key}</span>
+          )
+        }
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -107,6 +172,8 @@ class App extends Component {
         <GanttChart data={testData} />
         <h2 className="Chart-title">Timeline</h2>
         <TimelineChart data={testData} />
+        <h2 className="Chart-title">Heatmap</h2>
+        <Heatmap data={testData} />
       </div>
     );
   }
