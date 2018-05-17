b16bcaf775daa517d3f22765e34c7616344497d9
Shutaro Takimoto <shu@tak.im>
2018-05-17 11:25:43 +0900

    feat: Gantt と Timeline をべた書き

--- atoz-visualizer/src/App.js
+++ atoz-visualizer/src/App.js
@@ -44,15 +44,56 @@ function setupData(data) {
   });
 }
 
-function TimelineChart({data}) {
+function GanttChart({data}) {
   return (
     <BarChart data={setupData(data)} layout="vertical" width={1000} height={400} barCategoryGap="4">
       <YAxis type="category" dataKey="key" axisLine={false} tickLine={false} />
-      <XAxis type="number" allowDecimals={false} tickCount="20" />
+      <XAxis type="number" allowDecimals={false} tickCount={10} />
       <CartesianGrid horizontal={false} stroke="#ccc" strokeDasharray="5 5" />
       <Bar dataKey="prevDown" stackId="a" fill="transparent" isAnimationActive={false} />
       <Bar dataKey="downDiff" stackId="a" label={{ fill: 'white', fontSize: 9}} fill="darkslategray" stroke="darkslategray" isAnimationActive={false} />
       <Bar dataKey="press" stackId="a" label={{ fill: 'gray', fontSize: 9}} fill="transparent" stroke="darkslategray" isAnimationActive={false} />
+    </BarChart>
+  );
+}
+
+class TimelineLabel extends Component {
+  render() {
+    const x = this.props.x;
+    const y = this.props.y;
+    const width = this.props.width;
+    const height = this.props.height;
+    const text = this.props.content.props.text ? this.props.content.props.text : '';
+    const name = this.props.content.props.name;
+    console.log(this.props.content);
+
+    return (
+      <text x={x} y={y - 2} width={width} height={height} fill="darkslategray" fontSize="16" className="recharts-text recharts-label" textAnchor="middle">
+        <tspan x={x} dx={width} dy="0">{name}</tspan>
+        <tspan x={x} dx={width / 2} dy={height / 2 + 5} fontSize="10">{text}</tspan>
+      </text>
+    );
+  }
+}
+
+function TimelineChart({data}) {
+  const extData = setupData(data);
+  const flatData = extData.reduce((prev, current) => {
+    prev[current.key] = current.downDiff;
+    return prev;
+  }, {});
+
+  return (
+    <BarChart data={[flatData]} layout="vertical" width={1000} height={100} barCategoryGap="4" margin={{top: 15, right: 5, bottom: 5, left: 64}}>
+      <YAxis type="category" hide={true} />
+      <XAxis type="number" allowDecimals={false} tickCount={10} />
+      <CartesianGrid horizontal={false} stroke="#ccc" strokeDasharray="5 5" />
+      {
+        // TODO: 色
+        data.map(d => <Bar key={d.key} dataKey={d.key} stackId="a"
+                           label={<TimelineLabel name={d.key} text={flatData[d.key]} />}
+                           fill="transparent" stroke="darkslategray" isAnimationActive={false} />)
+      }
     </BarChart>
   );
 }
@@ -62,6 +103,8 @@ class App extends Component {
     return (
       <div className="App">
         <Header title={APP_NAME} />
+        <h2 className="Chart-title">Gantt</h2>
+        <GanttChart data={testData} />
         <h2 className="Chart-title">Timeline</h2>
         <TimelineChart data={testData} />
       </div>
